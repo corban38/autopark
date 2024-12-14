07568d68e646210472913d848583a12a1297cfff
Дмитрий Сапожников <corban71@yandex.ru>
2024-12-15 07:50:36 +0800

    Reapply "Comment Test"
    This reverts commit cea97824067c21b48a8ba9719aad2c46c8eed893.

--- Form1.cs
+++ Form1.cs
@@ -740,6 +740,7 @@ namespace autopark
 
         private void ItemsExpensesToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            // запуск новой формы
             using (FCars fCars = new FCars())
             {
                 if (fCars.ShowDialog() == DialogResult.OK)
